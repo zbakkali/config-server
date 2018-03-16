13a59decc1a277fd5096f4f83442ae1d908396af
Zoubair BAKKALI <zoubair.bakkali@laposte.net>
2018-03-16 04:27:07 +0100

    initial commit

---
+++ src/main/java/com/jad/config/server/configuration/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.jad.config.server.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.RequestHandlerSelectors.withClassAnnotation;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .build();
+    }
+}
